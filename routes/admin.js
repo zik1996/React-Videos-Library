74a3c8bc3bbeccda63fa60f0de89a89c1c5c88eb
zik1996 <zikrullahha2212@gmail.com>
2024-08-15 13:39:15 +0530

    added comments

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const adminController = require("../controllers/admin")
 
+// GET => /admin
 router.get("/admin", adminController.getAdmin)
 
+// POST => /add-admin
 router.post("/add-admin", adminController.postAdmin)
 
 module.exports = router;
